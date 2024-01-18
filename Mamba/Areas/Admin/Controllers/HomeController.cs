8077beac7c0d8a854c52b83658d96768a547bc3e
nihatab104 <146129654+1rehimzade@users.noreply.github.com>
2024-01-18 10:15:19 +0400

    ggg

---
+++ Mamba/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mamba.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    [AutoValidateAntiforgeryToken]
+    public class HomeController : Controller
+    {
+        [Authorize]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
